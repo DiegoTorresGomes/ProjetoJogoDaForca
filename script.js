9e6e9f9f0a4e9033cec21af05f90f6c8c6afd889
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-08-19 21:20:52 -0300

    ProjetoJogoDaForca

---
+++ script.js
@@ -0,0 +1,179 @@
+let palavraSecretaCategoria;
+let palavraSecretaSorteada;
+
+const palavras = [
+    palavra001={
+        nome: "IRLANDA",
+        categoria: "LUGARES"
+    },
+    palavra002={
+        nome: "INDONÉSIA",
+        categoria: "LUGARES"
+    },
+    palavra003={
+        nome: "BRASIL",
+        categoria: "LUGARES"
+    },
+    palavra004={
+        nome: "AFEGANISTÃO",
+        categoria: "LUGARES"
+    },
+    palavra005={
+        nome: "HUNGRIA",
+        categoria: "LUGARES"
+    },
+    palavra006={
+        nome: "BARCO",
+        categoria: "TRANSPORTE"
+    },
+    palavra007={
+        nome: "METRÔ",
+        categoria: "TRANSPORTE"
+    },
+    palavra008={
+        nome: "ÔNIBUS",
+        categoria: "TRANSPORTE"
+    },
+    palavra009={
+        nome: "TÁXI",
+        categoria: "TRANSPORTE"
+    },
+    palavra010={
+        nome: "UBER",
+        categoria: "TRANSPORTE"
+    },
+    palavra011={
+        nome: "MAMÃO",
+        categoria: "ALIMENTOS"
+    },
+    palavra012={
+        nome: "ABACATE",
+        categoria: "ALIMENTOS"
+    },
+    palavra013={
+        nome: "MAÇÃ",
+        categoria: "ALIMENTOS"
+    },
+    palavra014={
+        nome: "GOIABA",
+        categoria: "ALIMENTOS"
+    },
+    palavra015={
+        nome: "MELÃO",
+        categoria: "ALIMENTOS"
+    },
+    palavra016={
+        nome: "MARACUJÁ",
+        categoria: "ALIMENTOS"
+    },
+     palavra017={
+        nome: "JABUTICABA",
+        categoria: "ALIMENTOS"
+    },
+    palavra018={
+        nome: "JAMELÃO",
+        categoria: "ALIMENTOS"
+    },
+    palavra019={
+        nome: "TANGERINA",
+        categoria: "ALIMENTOS"
+    },
+    palavra020={
+        nome: "TOMATE",
+        categoria: "ALIMENTOS"
+    },
+    palavra021={
+        nome: "MONITOR",
+        categoria: "OBJETOS"
+    },
+    palavra022={
+        nome: "MOUSE",
+        categoria: "OBJETOS"
+    },
+    palavra023={
+        nome: "LAMPADA",
+        categoria: "OBJETOS"
+    },
+    palavra024={
+        nome: "CELULAR",
+        categoria: "OBJETOS"
+    },
+    palavra025={
+        nome: "ROTEADOR",
+        categoria: "OBJETOS"
+    },
+    palavra026={
+        nome: "CANECA",
+        categoria: "OBJETOS"
+    },
+    palavra027={
+        nome: "CADEIRA",
+        categoria: "OBJETOS"
+    },
+    palavra028={
+        nome: "TESOURA",
+        categoria: "OBJETOS"
+    },
+    palavra029={
+        nome: "LOUSA",
+        categoria: "OBJETOS"
+    },
+    palavra030={
+        nome: "CANETA",
+        categoria: "OBJETOS"
+    },
+    palavra031={
+        nome: "CACHORRO",
+        categoria: "ANIMAIS"
+    },
+    palavra032={
+        nome: "COBRA",
+        categoria: "ANIMAIS"
+    },
+    palavra033={
+        nome: "MACACO",
+        categoria: "ANIMAIS"
+    },
+    palavra034={
+        nome: "GATO",
+        categoria: "ANIMAIS"
+    },
+    palavra035={
+        nome: "JACARÉ",
+        categoria: "ANIMAIS"
+    },
+    palavra036={
+        nome: "CROCODILO",
+        categoria: "ANIMAIS"
+    },
+    palavra037={
+        nome: "ABELHA",
+        categoria: "ANIMAIS"
+    },
+    palavra038={
+        nome: "MOSCA",
+        categoria: "ANIMAIS"
+    },
+    palavra039={
+        nome: "MOSQUITO",
+        categoria: "ANIMAIS"
+    },
+    palavra040={
+        nome: "JAGUATIRICA",
+        categoria: "ANIMAIS"
+    },
+];
+
+function criarPalavraSecreta () {
+    const indexPalavra = parseInt(Math.random () * palavras.length);
+    
+    
+    
+    
+    palavraSecretaSorteada = palavras[indexPalavra].nome;
+    palavraSecretaCategoria = palavras[indexPalavra].categoria;
+    console.log(indexPalavra);
+
+
+}
+criarPalavraSecreta();
